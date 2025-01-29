b57b6ee3ce86abb8747b8fadad7894e8492499ce
Pedro <xtron44@gmail.com>
2025-01-29 00:39:41 -0300

    Descompactar uma sequência em variáveis separadas

---
+++ EstrututuraDeDados-E-Algoritmos/Descompactar-uma-sequencia-em-variaveis-separadas.py
@@ -0,0 +1,11 @@
+p=(3,4,5,6,7,8)
+x1,x2,x3,x4,x5,x6=p
+print(f"{x1},{x2},{x3},{x4},{x5},{x6}")
+
+dados=("Ola",1,3,4,(1,2,3,4))
+x1,x2,x3,x4,x5=dados
+print(f"{x1} , {x2} , {x3} , {x4} , {x5}")
+
+string="teste"
+x1,x2,x3,x4,x5=string
+print(f"{x1} , {x2} , {x3} , {x4} , {x5}")
